f15caf9e6143fa42d2aa23cedef4e3f269583318
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-12 02:26:43 -0400

    Update 1_NMT_With_Attention.py

--- 4_Natural_Language_Processing_with_Attention_Models/1_NMT_With_Attention.py
+++ 4_Natural_Language_Processing_with_Attention_Models/1_NMT_With_Attention.py
@@ -1,1 +1,7 @@
+import os
+import numpy as np
+import tensorflow as tf
+from collections import Counter
+from utils import sentences, train_data, val_data, english_vectorizer, portugese_vectorizer, masked_loss, masked_acc, tokens_to_text
+import w1_unittest
 
