1db0ae09008e18b3c0c2b2e45f7015bb29c6eae0
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-13 14:10:50 -0400

    Update 4_Word_Translation.py

--- 1_Natural_Language_Processing_with_Classification_and_Vector_Spaces/4_Word_Translation.py
+++ 1_Natural_Language_Processing_with_Classification_and_Vector_Spaces/4_Word_Translation.py
@@ -1,1 +1,85 @@
+import pdb 
+import pickle 
+import string
+import time 
+import nltk 
+import numpy as np
+from nltk.corpus import stopwords, twitter_samples
+from utils import cosine_similiarity, get_dict, process_tweet
+from os import getcwd
+import w4_unittest
 
+filePath
+
+en_embeddings_subset
+fr_embeddings_subset
+
+en_fr_train
+en_fr_test
+
+def get_matrics():
+
+    return X,Y
+
+
+def compute_loss():
+  
+    return loss
+
+
+def compute_gradient():
+
+    return gradient
+
+
+def align_embeddings():
+
+    return R
+
+
+def nearest_neighbor():
+
+    return k_idx
+
+
+def test_vocabulary():
+
+    return accuracy
+  
+
+def get_document_embedding():
+
+    return doc_embedding
+
+
+def get_document_vecs():
+
+    return document_vec_matrix, ind2Doc_dict
+
+
+def hash_value_of_vector():
+
+    return hash_value
+
+
+def make_hash_table():
+
+    return hash_table, id_table
+  
+
+def create_hash_id_tables():
+
+    return hash_tables, id_tables
+
+
+def approximate_knn():
+
+    return nearest_neighbors_ids
+
+
+
+
+
+
+
+  
