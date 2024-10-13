204308c7190cd6c81afafb90196259e5729992aa
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-13 14:18:57 -0400

    Update 1_Autocorrect.py

--- 2_Natural_Language_Processing_with_Probabilistic_Models/1_Autocorrect.py
+++ 2_Natural_Language_Processing_with_Probabilistic_Models/1_Autocorrect.py
@@ -1,1 +1,63 @@
+import re 
+from collections import Counter
+import numpy as np
+import pandas as pd
+import w1_unttest
+
+def process_data():
+
+    return words
+
+
+def get_count():
+
+    return word_count_dict
+
+
+def get_probs():
+
+    return probs
+
+
+def delete_letter(word,verbose=False):
+
+    return delete_l
+
+
+def switch_letter(word, verbose=False):
+
+    return switch_l
+
+
+def replace_letter(word,verbose=False):
+
+    return replace_l
+
+
+def insert_letter(word,verbose=False):
+
+    return insert_l
+
+
+def edit_one_letter(word,allow_stitches = True):
+
+    return set(edit_one_set)
+
+
+def edit_two_letters(word,allow_stitches=True):
+
+    return set(edit_two_set)
+
+
+def get_corrections(word,probs,vocab,n=2,verbose=False):
+
+    return n_best
+
+
+def min_edit_distanxe():
+
+    return D,med
+
+
+
 
