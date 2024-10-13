b4237e830e8b90d31e2f3652cf6125972b9579b0
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-13 14:31:41 -0400

    Update 3_Autocomplete.py

--- 2_Natural_Language_Processing_with_Probabilistic_Models/3_Autocomplete.py
+++ 2_Natural_Language_Processing_with_Probabilistic_Models/3_Autocomplete.py
@@ -1,1 +1,89 @@
+import math
+import random 
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download()
+import w3_inittest
+nltk.data.path.append()
 
+def split_to_sentences():
+
+    return sentences
+
+
+def tokenize_sentences():
+
+    return tokenized_sentences
+
+
+def get_tokenized_data():
+
+    return tokenized_sentences
+
+
+def count_words(tokenized_sentences):
+
+    return word_counts
+
+
+def get_words_with_nplus_frequency(tokenized_senteces,count_threshold):
+
+    return closed_vocab
+
+
+def replace_oov_wordS_by_unk():
+
+    return replaced_tokenized_sentences
+
+
+def preprocess_data():
+
+    return train_data_replaced, test_data_replaced, vocabulary
+
+
+def count_n_grams():
+
+    return n_grams
+
+
+def estimate_probability():
+
+    return probability
+
+
+def estimate_probabilities():
+
+    return probabilities
+
+
+def make_count_matrix():
+
+    return count_matrix
+
+
+def make_probability_matrix():
+
+    return prob_matrix
+
+
+def calculate_perplexity():
+
+    return perplexity
+
+
+def suggest_a_word():
+
+    return suggest, max_prob
+
+
+def get_suggestions():
+
+    return suggestions
+
+
+
+
+
+
+  
