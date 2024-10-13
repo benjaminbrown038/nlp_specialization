00ff388db899cfc71189da7b97e43bc15d08bf65
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-13 14:02:43 -0400

    Update 3_Assignment_Vector_Space_Models.py

--- 1_Natural_Language_Processing_with_Classification_and_Vector_Spaces/3_Assignment_Vector_Space_Models.py
+++ 1_Natural_Language_Processing_with_Classification_and_Vector_Spaces/3_Assignment_Vector_Space_Models.py
@@ -1,1 +1,37 @@
+import pickle 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import w3_unittest
+
+from utils import get_vectors
+
+word_embeddings
+
+def cosine_similiarity():
+
+king 
+queen
+cosine_similiarity()
+
+def euclidean_distance():
+    return d 
+
+def country():
+
+    return country 
+
+
+def get_accuracy():
+
+    return accuracy
+
+
+def comput_pca():
+
+    return X_reduced
+
+words
+X 
+
 
