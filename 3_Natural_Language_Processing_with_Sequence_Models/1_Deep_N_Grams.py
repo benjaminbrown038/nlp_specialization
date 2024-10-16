a8ea2e40ada6cfe62d84525ace619e2f1f1113a4
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-15 23:52:21 -0400

    Update 1_Deep_N_Grams.py

--- 3_Natural_Language_Processing_with_Sequence_Models/1_Deep_N_Grams.py
+++ 3_Natural_Language_Processing_with_Sequence_Models/1_Deep_N_Grams.py
@@ -1,1 +1,219 @@
+import os 
+
+import traceback
+import numpy as np
+import random as rnd
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input 
+from termcolor import colored
+
+import time 
+
+rnd.seed(32)
+
+import w1_unittest
+
+dirname = 
+filename = 
+lines = 
+counter 
+
+with open() as files:
+    for line in files:
+        pure_line 
+        if pure_line:
+            lines.append()
+
+n_lines = len(lines)
+print()
+
+print()
+
+text = 
+vocab = 
+
+
+
+print()
+print()
+
+line = 
+chars = 
+
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+ids = 
+print(ids)
+
+def line_to_tensor():
+
+    return ids
+
+tmp_ids = line_to_tensor()
+print()
+print()
+
+def text_from_ids():
+
+    return tf.strings.reduce_join()
+
+text_from_ids()
+
+train_lines = lines[:]
+eval_lines = lines[:]
+
+print()
+print()
+
+
+all_ids = 
+all_ids 
+
+ids_dataset = tf.data.Dataset.from_tensor_slices()
+print()
+
+seq_length
+data_generator 
+
+for seq in data_generator.take():
+    print()
+
+i = 1 
+for seq in data_generator.taken(2):
+    print()
+    i = i + 1
+
+def split_input_target():
+
+    return input_text, target_text
+
+def create_batch_dataset():
+
+    return dataset
+
+tf.random.set_seeD()
+dataset = 
+
+print()
+
+for input_example, target_example in dataset.take():
+    print()
+    print()
+    print()
+    print()
+
+
+BATCH_SIZE = 64
+dataset = create_batch_dataset()
+
+class GRULM():
+  
+    def __init__():
+
+    def call():
+
+
+vocab_size = 82 
+embedding_dim = 
+rnn_units = 
+model = 
+
+try:
+    model.build()
+    model.call()
+    model.summary()
+except:
+    print()  
+    traceback.print_exc()
+
+for int_example_batch, target_example_batch in datset.take(1):
+    print()
+    example_batch_predicition = model()
+    print()
+
+example_batch_predictions[0][99].numpy()
+last_character = tf.math.argmax(example_batch_predictions[0][99])
+print(last_character.numpy())
+
+sampled_indiced = tf.math.argmax(example_batch_predictions[0],axis=1)
+print(sampled_indices.numpy())
+
+print()
+print()
+print()
+
+def compile_model():
+    return model 
+
+EPCOHS = 
+model = compile_model()
+history = model.fit()
+
+out_dir
+try:
+    shutil.rmtree(output_dir)
+except OSError as e:
+    pass
+
+model.save_weights(output_dir)
+
+def log_perplexity():
+
+    return -log_ppx
+
+vocab_size = len()
+embedding_dim 
+rnn_units = 
+
+model = GRULUM
+model.build
+model.load_weights
+
+eval_text = 
+eval_ids = 
+input_ids, target_ids
+preds, status 
+log_ppx = 
+print()
+
+def temperature_random_sampling():
+
+    return tf.math.argmax()
+
+
+class GenerativeModel():
+
+      def __init__():
+
+      @tf.function
+      def generate_one_step():
+
+      def generate_n_chars():
+
+
+tf.random.set_seed()
+gen = GenerativeModel()
+
+print()
+print()
+print()
+
+tf.random.set_seed()
+gen = GenerativeModel()
+start 
+print()
+print()
+
+
+
+  
 
