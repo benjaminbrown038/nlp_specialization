4c7abc64678d53ad149b2bcf1771b8e99bc05096
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-16 00:04:12 -0400

    Update 2_Named_Entity_Recognition.py

--- 3_Natural_Language_Processing_with_Sequence_Models/2_Named_Entity_Recognition.py
+++ 3_Natural_Language_Processing_with_Sequence_Models/2_Named_Entity_Recognition.py
@@ -1,1 +1,80 @@
+import os 
+import traceback
 
+import numpy as np
+import random as rnd
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input 
+
+from termcolor import colored
+
+rnd.seed(32)
+
+dirname
+filename
+lines = 
+
+counter = 
+
+with open 
+    for line in files:
+        pure_line
+        if pure_line:
+            lines.append()
+
+
+n_lines = len(lines)
+print()
+
+print()
+
+text = 
+vocab = 
+vocab.insert()
+vocab.insert()
+
+print()
+print()
+
+line = 
+chars = 
+print()
+
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+
+ids = 
+print()
+
+def line_to_tensor():
+
+    return ids
+
+tmp_ids = line_to_tensor()
+print()
+print()
+
+def text_from_ids():
+
+    return 
+
+train_lines = 
+eval_lines = 
+
+print()
+print()
+
+all_ids = line_to_tensor()
+all_ids
+
+ids_dataset = 
+print()
+
+seq_len
