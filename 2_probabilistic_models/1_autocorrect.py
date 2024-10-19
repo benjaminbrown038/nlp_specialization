477c31a1ddaf8708e16f505e994fa7c35a17840c
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-19 12:33:52 -0400

    Update 1_autocorrect.py

--- 2_probabilistic_models/1_autocorrect.py
+++ 2_probabilistic_models/1_autocorrect.py
@@ -1,1 +1,137 @@
+import re
+from collections import Counter
+import numpy as np
+import pandas as pd 
+import w1_unittest
 
+def process_data():
+    return words
+
+word_l
+vocab
+print()
+print()
+
+w1_unittest.test_process_data()
+
+def get_count():
+    return word_count_dict
+
+word_count_dict = get_count()
+print()
+print()
+
+w1_unittest.test_get_count()
+
+def get_probs():
+    return probs
+
+probs 
+print()
+print()
+
+w1_unittest.test_get_probs
+
+def delete_letter():
+    return delete_l
+
+delete_word_l
+
+print()
+
+w1_unittest.test_delete_letter
+
+def switch_letter():
+    return switch_l
+
+print()
+
+w1_unittest.test_switch_letter()
+
+def replace_letter():
+    replace_l
+
+replace_l = replace_letter()
+
+print()
+w1_unittest.test_replace_letter()
+
+def inset_letter():
+    return insert_l
+
+insert_l
+print()
+
+w1_unittest.test_insert_letter
+def edit_one_letter():
+    return set(edit_one_set)
+
+tmp_word
+tmp_edit_one_set
+tmp_Edit_one_l 
+
+print()
+print()
+print()
+
+
+w1_unittest.test_edit_one()
+
+def edit_two_letters():
+    return set(edit_two_set)
+
+tmp_edit_two_set
+tmp_edit_two_l
+
+print()
+print()
+print()
+print()
+print()
+
+w1_unittest
+
+print()
+print()
+val1
+print()
+val2
+print()
+
+def get_corrections():
+    return n_best
+
+my_words
+tmp_corrections
+for :
+  print()
+
+print()
+
+w1_unittest.test_get_corrections()
+
+def min_edit_distance():
+
+    return D, med
+
+source
+target
+matrix, min_edits
+print()
+idx
+cols
+df
+print()
+
+source
+target
+matrix, min_edits
+print()
+idx
+idx
+cols
+cols.insert
+df
+print
+
+w1_unittest_
