7cd80cb1688bf7254b75a904907254051174a96b
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-19 11:57:58 -0400

    Update 3_vector_space_models.py

--- 1_classification_and_vector_spaces/3_vector_space_models.py
+++ 1_classification_and_vector_spaces/3_vector_space_models.py
@@ -1,1 +1,72 @@
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import w3_unittest
+from utils import get_vectors
+
+data = 
+data.columns
+data.head()
+
+word_embeddings
+len(word_embeddings)
+
+print()
+
+def cosine_similarity():
+    return cos
+
+king
+queen
+cosine_similarity()
+
+w3_unittest.test_cosine_similarity(cosine_similarity)
+
+def euclidean():
+    return d 
+
+euclidean()
+
+w3_unittest.test_euclidean(euclidean)
+
+def get_country():
+    return country
+
+get_country()
+
+w3_unittest.test_get_country()
+
+def get_accuracy():
+    return accuracy
+
+accuracy = 
+print()
+
+w3_unittest.test_get_accuracy()
+
+def compute_pca(X,n_components=2):
+    return X_reduced
+
+np.random.seed(1)
+
+X = 
+X_reduced 
+print()
+print()
+
+w3_unittest.test_compute_pca(compute_pca)
+
+words = 
+X 
+print()
+
+
+result 
+plt.scatter
+for i in word
+    plt.annotate
+
+plt.show
+
 
