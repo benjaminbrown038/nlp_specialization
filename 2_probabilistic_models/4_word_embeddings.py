9c62d8b4eef62765a2b08d9ab1b141476d421dd3
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-10-19 13:20:50 -0400

    Update 4_word_embeddings.py

--- 2_probabilistic_models/4_word_embeddings.py
+++ 2_probabilistic_models/4_word_embeddings.py
@@ -1,1 +1,64 @@
+import nltk
+from nltk.tokenize import word_tokenize
+import numpy as np
+from collections import Counter
+from utils2 import sigmoid, get_batches, compute_pca, get_dict
+import w4_unittest
+
+nltk.download('punkt')
+
+nltk.data.path.append('.')
+
+import re 
+with open()
+data
+data
+data
+data
+print()
+
+fdist
+print()
+print()
+
+word2Ind, Ind2eord
+V
+print
+print
+print
+
+def intialize_model():
+    return W1,W2, b1, b2
+
+tmp_N
+tmp_V
+tmp_W1, 
+
+assert
+assert
+print
+print
+print
+print
+
+def softmax():
+
+    return yhat
+
+tmp 
+tmp_sm
+display
+
+w4_unittest
+
+def forward_prop():
+
+    return z, h 
+
+tmp_N
+tmp_V
+tmp_x
+print
+
+
 
